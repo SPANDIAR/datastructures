3d239ab3aa647f4d5a8cae01fc133ebf61918c0f
sankar_pandiarajan <spandiarajan@apple.com>
2020-06-23 10:44:41 -0700

    modified soccer player

--- collections-general/src/io/spandiar/collections/SoccerPlayer.java
+++ collections-general/src/io/spandiar/collections/SoccerPlayer.java
@@ -2,21 +2,20 @@ package io.spandiar.collections;
 
 import java.util.Comparator;
 
-public class SoccerPlayer implements Comparator<SoccerPlayer>{
-	
+public class SoccerPlayer implements Comparator<SoccerPlayer> {
+
 	private int squadNumber;
 	private String position;
 	private String name;
 	private String currentClub;
 	private String nationality;
-	
-	
-	
+	private float height;
+	private float weight;
+
 	public SoccerPlayer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
 
 	public SoccerPlayer(int squadNumber, String position, String name, String currentClub, String nationality) {
 		super();
@@ -26,51 +25,72 @@ public class SoccerPlayer implements Comparator<SoccerPlayer>{
 		this.currentClub = currentClub;
 		this.nationality = nationality;
 	}
-	
-	
+
 	public int getSquadNumber() {
 		return squadNumber;
 	}
+
 	public void setSquadNumber(int squadNumber) {
 		this.squadNumber = squadNumber;
 	}
+
 	public String getPosition() {
 		return position;
 	}
+
 	public void setPosition(String position) {
 		this.position = position;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCurrentClub() {
 		return currentClub;
 	}
+
 	public void setCurrentClub(String currentClub) {
 		this.currentClub = currentClub;
 	}
+
 	public String getNationality() {
 		return nationality;
 	}
+
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
 
-	@Override
-	public int compare(SoccerPlayer p1, SoccerPlayer p2) {
-		
-		return Integer.compare(p1.getSquadNumber(), p2.getSquadNumber());
+	public float getHeight() {
+		return height;
 	}
 
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public int compare(SoccerPlayer p1, SoccerPlayer p2) {
+
+		return Integer.compare(p1.getSquadNumber(), p2.getSquadNumber());
+	}
 
 	@Override
 	public String toString() {
 		return "SoccerPlayer [squadNumber=" + squadNumber + ", name=" + name + "]";
 	}
-	
-	
 
 }
